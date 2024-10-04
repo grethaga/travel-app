2a0c4690d19c59eee74a299a994016e26db0e4b2
grethagall <100848664+grethagall@users.noreply.github.com>
2024-10-04 16:12:32 +0200

    add hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:fap-28 lg:flex-row border-2 border-red-500">
+      <div className="hero-map"></div>
+
+      <div className="relative z-20 flex flex-1 flex-col lg:w-1/2">
+        <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">Putuk Kuno Camp Area</h1>
+        <p className="regular-16 text-grey-30 lg:max-w-[520px]">
+          We want to be on each of your journeys seeking the satisfaction of
+          seeing the incorruptible beauty of nature. We can help you on an
+          adventure around the world in just one app
+        </p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
